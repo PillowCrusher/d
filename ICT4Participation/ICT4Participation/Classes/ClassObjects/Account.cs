66de1bc1a02045d56fed9fee8bb533a14e334793
Verkuijlen,Kevin K.A.J <kevin.verkuijlen@student.fontys.nl>
2016-04-14 10:06:02 +0200

    all constructors added

--- ICT4Participation/ICT4Participation/Classes/ClassObjects/Account.cs
+++ ICT4Participation/ICT4Participation/Classes/ClassObjects/Account.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace ICT4Participation.Classes.ClassObjects
 {
-    class Account
+    public class Account
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public Account(string username, string password, string email)
+        {
+            if (username == null)
+            {
+                throw new ArgumentNullException("username", "username is empty");
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("passowrd", "password is empty");
+            }
+            if (email == null)
+            {
+                throw new ArgumentNullException("email", "email is empty");
+            }
+            this.Username = username;
+            this.Password = password;
+            this.Email = email;
+        }
     }
 }
