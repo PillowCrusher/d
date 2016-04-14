4901d919dc757a40e3dcf95dfd50f788f6db0965
Laurent Lardenois <laurentlardenois@hotmail.nl>
2016-04-14 15:30:50 +0200

    Kleine edits in Chatmessage en Review

--- ICT4Participation/ICT4Participation/Classes/ClassObjects/Review.cs
+++ ICT4Participation/ICT4Participation/Classes/ClassObjects/Review.cs
@@ -8,11 +8,13 @@ namespace ICT4Participation.Classes.ClassObjects
 {
     public class Review
     {
-        public string Rating { get; set; }
-        public string Comment { get; set; }
+        public Volunteer Volunteer { get; }
+        public string Rating { get; }
+        public string Comment { get; }
 
-        public Review(string rating, string comment, HelpRequest helprequest)
+        public Review(string rating, string comment, Volunteer volunteer)
         {
+            this.Volunteer = volunteer;
             this.Rating = rating;
             this.Comment = comment;
         }
