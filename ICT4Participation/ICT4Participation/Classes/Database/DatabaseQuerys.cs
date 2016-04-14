4ad1b406764d340ae8099cd21e1561687b687616
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-04-14 15:30:14 +0200

    Iets meer...

--- ICT4Participation/ICT4Participation/Classes/Database/DatabaseQuerys.cs
+++ ICT4Participation/ICT4Participation/Classes/Database/DatabaseQuerys.cs
@@ -8,11 +8,13 @@ namespace ICT4Participation.Classes.Database
 
         static DatabaseQuerys()
         {
-            Query["GetUserLogin"] = "SELECT * FROM \"ACCOUNT\" WHERE USERNAME = :username AND PASSWORD = :password";
+            Query["InsertAccount"] = "INSERT INTO \"Account\" (ID, Username, Password, Email) values (ACCOUNT_SEQ.nextval, :harry, :password, :email);";
+            Query["InsertVolunteer"] = "INSERT INTO \"Account\" (ID, Username, Password, Email) values (ACCOUNT_SEQ.nextval, :harry, :password, :email);";
+            Query["GetUserLogin"] = "SELECT Username, Password FROM \"ACCOUNT\" WHERE USERNAME = :username AND PASSWORD = :password";
             Query["GetAllHelpRequests"] = "SELECT * FROM \"HELPREQUEST\"";
             Query["GetUserHelpRequests"] = "SELECT * FROM \"HELPREQUEST\" WHERE NEEDYID = :needyid";
             Query["GetChatMessagesFromHelprequest"] = "SELECT * FROM \"CHATMESSAGE\" WHERE HELPREQUEST_ID = :helprequest_id";
-            Query["Blargh"] = "";
+            
         }
     }
 }
