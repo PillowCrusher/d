7304c8353d5645ab5193f86340dbe268ffe1e3ad
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-04-21 13:55:33 +0200

    Queries aangepast

--- ICT4Participation/ICT4Participation/Classes/Database/DatabaseQuerys.cs
+++ ICT4Participation/ICT4Participation/Classes/Database/DatabaseQuerys.cs
@@ -10,7 +10,7 @@ namespace ICT4Participation.Classes.Database
         {
             Query["GetAccountID"] = "SELECT ACCOUNT_SEQ.nextval from dual;";
             Query["InsertAccount"] = "INSERT INTO \"Account\" (ID, Username, Password, Email) values (:id, :username, :password, :email);";
-            Query["InsertUser"] = "INSERT INTO \"User\" (ID, NAME, ADRESS, CITY, PHONENUMBER, HASDRIVINGLICENCE, HASCAR, DEREGISTRATIONDATE, OVPOSSIBLE) values (:id, :name, :adress, :city, :phonenumber, :hasdrivinglicence, :hascar, :deregistrationdate, :ovpossible,);";
+            Query["InsertUser"] = "INSERT INTO \"User\" (ID, NAME, ADRESS, CITY, PHONENUMBER, HASDRIVINGLICENCE, HASCAR, OVPOSSIBLE) values (:id, :name, :adress, :city, :phonenumber, :hasdrivinglicence, :hascar, :ovpossible,);";
             Query["InsertVolunteer"] = "INSERT INTO \"Volunteer\" (ID, DATEOFBIRTH, PHOTO, VOG) values (:id, :dateofbirth, :photo, :vog);";
             Query["InsertNeedy"] = "INSERT INTO \"Needy\" (ID, RFID) values (:id, :rfid);";
             Query["InsertAdmin"] = "INSERT INTO \"Admin\" (ID, RFID) values (:id, :rfid);";
@@ -20,8 +20,16 @@ namespace ICT4Participation.Classes.Database
             Query["InsertUserHelprequest"] = "INSERT INTO \"UserHelprequest\" (UserID, HelpRequestID) values (:userid, :helprequestid);";
             Query["InsertReport"] = "INSERT INTO \"Report\" (Reporter, Reported, Reason) values (:reporter, :reported, :reason);";
 
-            Query["GetUserLogin"] = "SELECT Username, Password FROM \"Account\" WHERE USERNAME = :username AND PASSWORD = :password";
+            Query["GetUserLogin"] =
+                "select * from \"Account\" a left join \"User\" u on a.ID = u.ID left join \"Volunteer\" v on u.ID = v.ID left join \"Needy\" n on u.ID = n.ID where a.Username = :username and a.Password = :password;";
+            Query["GetUserLoginByRFID"] =
+                "select * from \"Account\" a left join \"User\" u on a.ID = u.ID left join \"Volunteer\" v on u.ID = v.ID left join \"Needy\" n on u.ID = n.ID where n.RFID = :rfid;";
+            Query["GetAdminLogin"] =
+                "select * from \"Account\" a left join \"Admin\" ad on a.ID = ad.ID where a.Username = :username and a.Password = :password and n.RFID = :rfid;";
             Query["GetAllHelpRequests"] = "SELECT * FROM \"Helprequest\"";
+            Query["UpdateAccount"] = "update \"Account\" set Username = :username, Password = :password, Email = :email;";
+            Query["UpdateUser"] = "update \"User\" set Name = :name, Adress = :adress, City = :city, Phonenumber = :phonenumber, Hasdrivinglicence = :hasdrivinglicence, Hascar = :hascar;";
+            Query["UpdateVolunteer"] = "update \"Volunteer\" set DateOfBirth = :dateofbirth, photo = :photo, City = :vog;";
             Query["GetUserHelpRequests"] = "SELECT * FROM \"Helprequest\" WHERE NEEDYID = :needyid";
             Query["GetChatMessagesFromHelprequest"] = "SELECT * FROM \"ChatMessage\" WHERE HELPREQUEST_ID = :helprequest_id";
             
