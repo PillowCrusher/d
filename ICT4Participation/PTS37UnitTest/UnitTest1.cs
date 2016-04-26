dbe1ee4e5748edee10c6ba4a04539c167388dc1e
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-04-26 10:23:29 +0200

    Harry

--- ICT4Participation/PTS37UnitTest/UnitTest1.cs
+++ ICT4Participation/PTS37UnitTest/UnitTest1.cs
@@ -2,6 +2,8 @@
 using ICT4Participation.Classes.Database;
 using ICT4Participation.Classes.Intelligence;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phidgets;
+using Phidgets.Events;
 
 namespace PTS37UnitTest
 {
@@ -9,6 +11,7 @@ namespace PTS37UnitTest
     public class UnitTest1
     {
         private Administration _administration;
+        private RFID rfid;
         [TestInitialize]
         public void Initialize()
         {
@@ -19,6 +22,25 @@ namespace PTS37UnitTest
         public void Test_ConnectionString()
         {
             Assert.AreEqual(true, DatabaseManager.CheckConnection());
+        }
+
+        [TestMethod]
+        public void Test_InloggenAlsGebruiker()
+        {
+            string username = "Harrie";
+            string password = "HarriePotter";
+            string false_username = "falseusername";
+            string false_password = "falsepassword";
+            string rfid = "2800a7c372";
+            _administration.Login(username, password);
+            _administration.Login(false_username, false_username);
+            _administration.LoginWithRfid(rfid);
+        }
+
+        [TestMethod]
+        public void Test_InloggenMetRFID()
+        {
+            
         }
     }
 }
