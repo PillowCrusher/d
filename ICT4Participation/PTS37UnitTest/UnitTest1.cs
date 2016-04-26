25be8abff330d9bbb348f1fb0dd69642343dcadc
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-04-26 09:30:29 +0200

    efwl

--- ICT4Participation/PTS37UnitTest/UnitTest1.cs
+++ ICT4Participation/PTS37UnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ICT4Participation.Classes.Database;
+using ICT4Participation.Classes.Intelligence;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PTS37UnitTest
@@ -7,6 +8,13 @@ namespace PTS37UnitTest
     [TestClass]
     public class UnitTest1
     {
+        private Administration _administration;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _administration = new Administration();
+            
+        }
         [TestMethod]
         public void Test_ConnectionString()
         {
