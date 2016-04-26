3b4c426bcc1a224dd1a787cfae80ccd754b0547e
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-04-26 10:54:17 +0200

    Added test classes

---
+++ ICT4Participation/PTS37UnitTest/Chatten.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PTS37UnitTest
+{
+    [TestClass]
+    public class Chatten
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
