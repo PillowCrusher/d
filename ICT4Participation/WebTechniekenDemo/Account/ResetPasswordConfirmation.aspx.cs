d3cc7aa54c13f012e37b1a731b2f967d8640622b
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-05-17 10:13:41 +0200

    Added demo

---
+++ ICT4Participation/WebTechniekenDemo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebTechniekenDemo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ICT4Participation/WebTechniekenDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTechniekenDemo.Startup))]
+namespace WebTechniekenDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
