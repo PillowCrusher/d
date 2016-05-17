777313dbe4c52221fabd6f4654f2e54aa9e4224a
Verkuijlen,Kevin K.A.J <kevin.verkuijlen@student.fontys.nl>
2016-05-17 13:47:03 +0200

    Unsubscribe toegevoegd

--- ICT4Participation/ICT4Participation/Classes/Database/Database.cs
+++ ICT4Participation/ICT4Participation/Classes/Database/Database.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ICT4Participation.Classes.ClassObjects;
 using Oracle.ManagedDataAccess.Client;
 
 namespace ICT4Participation.Classes.Database
 {
     public class Database
     {
+        public void UnSubscribe(User user)
+        {
+            try
+            {
+                DateTime deRegistrationDate = DateTime.Now;
+                OracleParameter[] userParameter =
+                {
+                new OracleParameter("deregistrationdate", deRegistrationDate),
+                new OracleParameter("id", user.ID)
+                };
+                DatabaseManager.ExecuteInsertQuery(DatabaseQuerys.Query["UnsubscribeUser"], userParameter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
