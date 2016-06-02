0fb25deb9779ebfd8c3d6936b1c25937cab0c2d5
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-06-02 14:05:13 +0200

    ofek

---
+++ ICT4 Participation MVC/ICT4 Participation MVC/Models/TransportationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Participation.Classes.ClassObjects
+{
+    public enum TransportationType
+    {
+        NVT,
+        Trein,
+        Bus,
+        Auto,
+        Fiets,
+        Benenwagen
+    }
+}
