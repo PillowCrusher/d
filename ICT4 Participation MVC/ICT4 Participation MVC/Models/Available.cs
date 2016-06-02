cee929323ed287dd4d1b82812f6c07c6fbedf0ca
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-06-02 13:58:28 +0200

    Added available

---
+++ ICT4 Participation MVC/ICT4 Participation MVC/Models/Available.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICT4_Participation_MVC.Models
+{
+    public class Available
+    {
+        public DateTime TimeAvailable { get; protected set; }
+
+        public Available(DateTime available)
+        {
+            this.TimeAvailable = available;
+        }
+    }
+}
