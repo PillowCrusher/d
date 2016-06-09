fec2446c13a2811a475c23b4a76fb80c879617ad
Soree,Kevin K.P.G. <seth0s@hotmail.com>
2016-06-09 14:44:59 +0200

    jaja

--- ICT4 Participation ASP/ICT4 Participation ASP/WebForms/LoginStandard.aspx.cs
+++ ICT4 Participation ASP/ICT4 Participation ASP/WebForms/LoginStandard.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ICT4_Participation_ASP.Models;
+using ICT4_Participation_ASP.Models.Administrations;
 
 namespace ICT4_Participation_ASP.WebForms
 {
@@ -12,6 +13,17 @@ namespace ICT4_Participation_ASP.WebForms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
+
+        private void Page_Error(object sender, EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+            Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax", true);
+        }
+
+        protected void btnLogin_OnClick(object sender, EventArgs e)
+        {/*
             Administration a = new Administration();
             a.Login(inputUsername.Text, inputPassword.Text);
             if (a.User is Volunteer)
@@ -22,16 +34,10 @@ namespace ICT4_Participation_ASP.WebForms
             {
                 Response.Redirect("NeedyHome.aspx");
             }
-            if(a.User is Admin)
+            if (a.User is Admin)
             {
                 Response.Redirect("AdminHome.aspx");
-            }
-        }
-
-        private void Page_Error(object sender, EventArgs e)
-        {
-            Exception exc = Server.GetLastError();
-            Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax", true);
+            }*/
         }
     }
 }
