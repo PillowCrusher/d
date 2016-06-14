10e7509090bfd9c11144025042d4eeabe0b0d78c
Theun Schut <theun_96@hotmail.com>
2016-06-14 21:34:17 +0200

    admin login

--- ICT4 Participation ASP/ICT4 Participation ASP/AdminPage.aspx.cs
+++ ICT4 Participation ASP/ICT4 Participation ASP/AdminPage.aspx.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ICT4_Participation_ASP.Models.Accounts;
+using ICT4_Participation_ASP.Models.Handlers;
 
 namespace ICT4_Participation_ASP
 {
     public partial class AdminPage : System.Web.UI.Page
     {
+        private AdminHandler _adminHandler;
+        private Admin _currentAdmin;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["LoggedUser"] is Admin)
+            {
+                _currentAdmin = (Admin)Session["LoggedUser"];
+                _adminHandler = new AdminHandler();
+            }
+            else
+            {
+                Response.Redirect("LoginStandard.aspx");
+            }
         }
     }
 }
