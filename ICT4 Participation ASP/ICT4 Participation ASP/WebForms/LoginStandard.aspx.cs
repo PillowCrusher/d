3538ffa7e80fae9c918f6c46499c9edaf23e65dc
Verkuijlen,Kevin K.A.J <kevin.verkuijlen@student.fontys.nl>
2016-06-16 12:04:19 +0200

    login exception

--- ICT4 Participation ASP/ICT4 Participation ASP/WebForms/LoginStandard.aspx.cs
+++ ICT4 Participation ASP/ICT4 Participation ASP/WebForms/LoginStandard.aspx.cs
@@ -50,7 +50,7 @@ namespace ICT4_Participation_ASP.WebForms
             }
             catch (Exception ex)
             {
-                Page.ClientScript.RegisterStartupScript(this.GetType(), "Script", "<script>alert('sad face');</script>");
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Script", "<script>alert('Er ging iets mis tijdens het inloggen, controleer of uw gebruikersnaam en wachtwoord kloppen');</script>");
             }
 
 
